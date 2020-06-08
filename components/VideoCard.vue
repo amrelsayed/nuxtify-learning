1b58918b28afb2a69a11bd4240f57df67001ca98
Amr Elsayed <amr-elsayed@outlook.com>
2020-06-08 22:51:43 +0200

    video cards in home page

---
+++ components/VideoCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card max-width="340px" 
+    hover 
+    class="ma-2">
+    <!-- :to="{ name: 'video-watch', params: {id: video.id }}"> -->
+    <v-img :src="video.thumb" />
+    <v-card-title>{{ video.name }}</v-card-title>
+    <v-card-text>
+        <!-- <div class="green--text" v-if="isPlayed(video.id)">
+            <font-awesome-icon icon="check" /> Played
+        </div> -->
+    </v-card-text>
+
+    <v-card-actions>
+        <span v-for="tag in video.tags" :key="`${video.id}-${tag.id}`">
+            <v-btn color="green lighten-2"
+            class="mr-2"
+            small
+            @mousedown.stop
+            :to="{ name: 'tag', params: {id: tag.id}}">
+            {{ tag.name }}
+            </v-btn>
+        </span>
+    </v-card-actions>
+    </v-card>
+
+</template>
+<script>
+export default {
+    props: ['video']
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-        home page
+    <div class="home">
+        <div class="display-2 ma-4 d-flex justify-center">
+          All Videos
+        </div>
+
+        <div class="d-flex flex-wrap">
+          <div v-for="video in videos" :key="video.id">
+            <videocard :video="video"></videocard>
+          </div>
+        </div>
     </div>
 </template>
 
 <script>
-
+import videocard from '../components/VideoCard'
 export default {
   components: {
-    
-  }
+    videocard
+  },
+    head: {
+        title: 'Nuxtify Learning - Home'
+    },
+    async asyncData({ $axios }) {
+        let videos = await $axios.$get(`videos`)
+        
+        return {
+          videos
+        }
+    }
 }
 </script>
 
